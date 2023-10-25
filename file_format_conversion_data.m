27ed8705c61eff639c86d1abc9aa159e9748f226
PrzemyslawHausman <82411903+PrzemyslawHausman@users.noreply.github.com>
2023-10-25 13:58:47 +0200

    Delete file_format_conversion_data.m

--- file_format_conversion_data.m
+++
@@ -1,9 +0,0 @@
-clear
-temp = regexp(fileread('date_text6.txt'), '\r?\n', 'split');
-output = vertcat(temp{:});
-n = size(output,1);
-for i=1:n
-data(i) = convertCharsToStrings(output(i,:));
-end
-save('date.mat', 'data');
-
